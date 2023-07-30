5446c47deafe58f48a0bff0e6c700c3db5772258
oldflag <oldflag2333333@gmail.com>
2023-07-30 20:20:25 +0800

    优化边界逻辑

--- src/hello.go
+++ src/hello.go
@@ -7,7 +7,7 @@ func main() {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
 
 	for i := 0; i < 12; i++ {
-		numbers = slice.DeleteSliceElement(numbers, 2)
+		numbers = slice.DeleteSliceElement(numbers, 0)
 		fmt.Println(numbers, len(numbers), cap(numbers))
 	}
 
--- src/slice/SliceUtil.go
+++ src/slice/SliceUtil.go
@@ -4,14 +4,16 @@ func DeleteSliceElement[T any](sl []T, index int) []T {
 	l := len(sl)
 	c := cap(sl)
 
-	if index > l {
+	if index > l || l == 0 {
 		return sl
 	}
 
 	if l-1 <= c>>2 {
 		// 1/4 时缩容一半
 		result := make([]T, l-1, (c+1)/2)
-		copy(result, sl[:index])
+		if index != 0 {
+			copy(result, sl[:index])
+		}
 		// 如果删除的是最后一个元素就不用拷贝另外一部分
 		if l > index {
 			copy(result[index:], sl[index+1:])
